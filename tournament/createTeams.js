0afbbc45eb2393de3c0a33edcdfa0d8471320d46
sblom <sblom@zigwebsoftware.nl>
2018-01-13 01:40:13 +0100

    Extra console logging.

--- tournament/createTeams.js
+++ tournament/createTeams.js
@@ -1,36 +1,31 @@
 exports.createTeams = function (message, args, pool) {
-    console.log('Creating teams');
-    var PythonShell = require('python-shell');
-
-    var options = {
-        mode: 'text',
-        args: ['/tmp/players.csv']
-    };
-
     var fs = require('fs');
     fs.exists('/tmp/players.csv', function() {
-        console.log('file exists');
+        console.log('/tmp/players.csv exists.');
+
+        var PythonShell = require('python-shell');
+
+        var options = {
+            mode: 'text',
+            args: ['versioninfo', '/tmp/players.csv']
+        };
+        PythonShell.run('dotaTeamMaker.py', options, function (error, results) {
+            if (error) {
+                console.error(error.toString());
+                return;
+            }
+
+            console.log(results);
+
+            if (results && results[0] === 'Please submit a (valid) playerlist file.\r') {
+                message.channel.send(`Please submit a (valid) playerlist file`);
+            }
+
+            message.channel.send(`Dota TeamMaker has run.`);
+
+            var importTeams = require('./importTeams');
+            importTeams.importTeams(message, args, pool, results);
+
+        });
     });
-
-    PythonShell.run('dotaTeamMaker.py', options, function (error, results) {
-        if (error) {
-            console.error(error.toString());
-            return;
-        }
-
-        console.log(results);
-
-        if (results && results[0] === 'Please submit a (valid) playerlist file.\r') {
-            message.channel.send(`Please submit a (valid) playerlist file`);
-        }
-
-        message.channel.send(`Dota TeamMaker has run.`);
-
-        var importTeams = require('./importTeams');
-        importTeams.importTeams(message, args, pool, results);
-
-        return;
-    });
-
-    return;
 }
