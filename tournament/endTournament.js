ac2d7af5acdd9e5732f74bfc69451c2014c59d2d
sblom <sblom@zigwebsoftware.nl>
2019-02-12 17:56:02 +0100

    Update for the winning team.

--- tournament/endTournament.js
+++ tournament/endTournament.js
@@ -11,6 +11,16 @@ exports.endTournament = function (message, args, pool) {
                 return;
             }
 
+            let kayzrJoinedRole = message.guild.roles.find(role => role.name === 'Joined Kayzr');
+            kayzrJoinedRole.members.forEach(function (guildMember) {
+                guildMember.removeRole(kayzrJoinedRole);
+            });
+
+            let kayzrAlphaWinnersRole = message.guild.roles.find(role => role.name === 'Kayzr Alpha Winners');
+            kayzrAlphaWinnersRole.members.forEach(function (guildMember) {
+                guildMember.removeRole(kayzrAlphaWinnersRole);
+            });
+
             let kayzrPlayersRole = message.guild.roles.find(role => role.name === 'Kayzr Players');
             let notice = `${kayzrPlayersRole}! De spelerslijst is weer geleegd, en vanaf nu kan iedereen zich dus weer inschrijven!`;
 
@@ -20,11 +30,10 @@ exports.endTournament = function (message, args, pool) {
                 message.channel.send(notice);
             }
 
-            let kayzrJoinedRole = message.guild.roles.find(role => role.name === 'Joined Kayzr');
-            kayzrJoinedRole.members.forEach(function (guildMember) {
-                guildMember.removeRole(kayzrJoinedRole);
-            });
-
+            if (args.includes('showwinningteam')) {
+                let showWinningTeam = require('./showWinningTeam');
+                showWinningTeam.showWinningTeam(message, args, pool);
+            }
         });
     });
 };
--- tournament/showWinningTeam.js
+++ tournament/showWinningTeam.js
@@ -2,19 +2,35 @@ const Discord = require('discord.js');
 
 exports.showWinningTeam = function(message, args, pool)
 {
-    if (args.length < 1 || args.length > 3 || isNaN(args[0])) {
-        message.channel.send(`Not enough arguments. I just need 1 number, of the winning team.`);
+    if (args.length < 1 || args.length > 4) {
+        message.channel.send(`Incorrect number of arguments. I just need 1 number, of the winning team.`);
+        return;
     }
 
-    var winningTeam = parseInt(args[0]);
+    let winningTeam = parseInt(args[0]);
+    if (!isNaN(args[0])) {
+        winningTeam = parseInt(args[0]);
+    } else if (!isNaN(args[1])) {
+        winningTeam = parseInt(args[1]);
+    } else if (!isNaN(args[2])) {
+        winningTeam = parseInt(args[2]);
+    }
 
-    var sql = `SELECT * 
+    let sql = `SELECT teamID, 
+            team_player.playerID as playerID, 
+            playername, 
+            position, 
+            kayzrname, 
+            mmr, 
+            preferred_positions, 
+            preferred_captain 
         FROM team_player
-        LEFT JOIN player on player.playername = team_player.playerID
+        LEFT JOIN player on player.playerID = team_player.playerID
         LEFT JOIN team on team.ID = team_player.teamID
         WHERE team.ID = ?
-        ORDER BY team_player.position`
+        ORDER BY team_player.position`;
 
+    console.log(winningTeam);
     pool.getConnection(function(error, connection) {
         connection.query({
             sql: sql,
@@ -35,14 +51,14 @@ exports.showWinningTeam = function(message, args, pool)
                 return;
             }
 
-            var embed = new Discord.RichEmbed()
+            let embed = new Discord.RichEmbed()
                 .setTitle('BeNeDota Player Draft Winning Team')
                 .setFooter(`BeNeDota Kayzr Player Draft Team Info`)
                 .setThumbnail('https://benedota.com/thumbs/assets/images/benedota_transp_crop-217x250.png')
                 .setTimestamp()
                 .setDescription(`May I present to you, the winners of this weeks BeNeDota Player Draft!`)
 
-            var colors = [
+            let colors = [
                 '3375ff',
                 '66ffbf',
                 'bf00bf',
@@ -56,12 +72,21 @@ exports.showWinningTeam = function(message, args, pool)
                 'a46900'
             ];
 
+            let kayzrAlphaWinnersRole = message.guild.roles.find(role => role.name === 'Kayzr Alpha Winners');
+
             results.forEach(function(player) {
                 embed.addField(
                     `${player['position']}. ${player['playername']} ${player.kayzrname ? '(Kayzr: ' + player.kayzrname + ')' : ''}`,
                     `Preferred role: ${player['preferred_positions']}, Preferred captain: ${player['preferred_captain'] == 1 ? 'Yes' : 'No'}`
                 );
                 embed.setColor(colors[player['team_ID']]);
+                let guildMember = message.guild.members.find('id', player['playerID']);
+                if (guildMember) {
+                    guildMember.addRole(kayzrAlphaWinnersRole)
+                        .catch(error => {
+                            logger.err(error)
+                        });
+                }
             });
 
             if (args[1] === 'live') {
@@ -70,14 +95,6 @@ exports.showWinningTeam = function(message, args, pool)
                 message.channel.send({embed});
             }
 
-            if (args[1] == 'reset'
-                || args[2] == 'reset') {
-                var endTournament = require('./endTournament.js');
-                endTournament.endTournament(message, args, pool);
-            }
-
-            return;
         });
     });
-    return;
-}
+};
