81a078e350ce6136a2e5ed1efc631bacf4f449a4
stephanblom <stephan_blom@live.nl>
2019-03-05 21:01:50 +0100

    Update help.js

--- bot/help.js
+++ bot/help.js
@@ -1,72 +1,50 @@
 const Discord = require('discord.js');
 
 exports.sendHelp = function (message, args) {
-    var helpCommands = {
-        '!register (or !update) <mmr> <preferred captain (Yes/No)> <position (1,2,3,4,5,Any)> <position (optional)> ...'
-            : `Registers you to the bot, or updates your data, for example:
-                \`!register 2700 Nee 5 4\`,
-                \`!register 6543 Yes 1 2\`,
-                \`!register 9001 Ja Any\``,
-        '!playerlist': `Shows the people that have \`!join\` ed the next tournament`,
-        '!join': `Enters you in the next tournament`,
-        '!leave': `Remove your entry for the next tournament`,
-        '!status': `Shows the info you are currently registered with.`,
-        '!kayzrname (name/clear)': `If your Kayzr profile name is different than the username on Discord, use this to set your Kayzr name. This way, we can add you to the Kayzr team`
-    }
-
-    var embed = new Discord.RichEmbed()
-        .setColor('#a52b16')
-        .attachFile(`./images/dota2.png`)
-        .setThumbnail(`attachment://dota2.png`)
-        .setTimestamp()
-        .setTitle(`Available commands`)
-        .setDescription(`These are the currently available commands to use`)
-        .setFooter(`BeNeDota Kayzr Player Draft Help`);
-
-    for (var key in helpCommands) {
-        embed.addField(key, helpCommands[key]);
-    }
-
+    message.author.send(
+        'Hi, here are the commands for the Kayzr Player Bot, for BeNeDota: \n'
+        + '\n'
+        + '**!register (or !update)** <mmr> <preferred captain (Yes/No)> <position (1,2,3,4,5,Any)> *<position (optional, multiple allowed)>* \n'
+        + 'This command registers you to the bot, or updates your data, for example: \n'
+        + '`!register 2700 Nee 5 4` \n'
+        + '**!playerlist:** \n'
+        + 'This command shows the people that have `!join`ed the next tournament. \n'
+        + '**!join** \n'
+        + 'This will enter you in the next tournament. \n'
+        + '**!leave** \n'
+        + 'Remove your entry for the next tournament. \n'
+        + '**!status** \n'
+        + 'Shows the info you are currently registered with. \n'
+        + '**!kayzrname** <your name> \n'
+        + 'If your Kayzr profile name is different than the username on Discord, use this to set your Kayzr name. This way, we can add you to your Kayzr team. \n'
+    ); 
+        
     if (message.member.roles.find("name", "Admin")
         || message.member.roles.find("name", "Staff")
     ) {
-        embed.addField(
-            '!registerPlayer <username (mention)> <mmr> <position> <preferred captain>',
-            'Registers/Updates the user to the bot'
-        );
-        embed.addField(
-            '!joinPlayer <username (mention)>',
-            'Joins the user to the next tournament'
-        );
-        embed.addField(
-            '!leavePlayer <username (mention)>',
-            'Removes the entry of the user for the next tournament'
-        );
-        embed.addField(
-            '!createtournament (or !createteams)',
-             `This starts the process of creating the teams.`
-        );
-        embed.addField(
-            '!showteams <live (optional)>',
-            `This shows the teams created during !createtournament. `
-             + `When used with 'live' parameter, this will show the teams in the Kayzr channel.`
-        );
-        embed.addField(
-            '!endtournament <live (optional> <showWinningTeam (optional> <winningTeamNumber (required when using showWinningTeam)>',
-            `This ends the current tournament, resets the "Joined Kayzr" roles and unjoins everyone from the playerlist. `
-            + `This also unsets Kayzr Alpha Winner roles. `
-            + `**live** Sends the message to the #kayzr channel. `
-            + `**showWinningTeam #** Gives the winners role to the winning team. `
-        );
-        embed.addField(
-            '!showwinningteam <teamnumber> <live (optional)> <updateRoles (optional)',
-            `This shows the winning team, and updates the winners role accordingly if *updateroles* is given. `
+        message.author.send(
+            '***Admin commands*** \n'
+            + '**!registerPlayer** <username (mention)> <mmr> <preferred captain (Yes/No)> <position (1,2,3,4,5,Any)> *<position (optional, multiple allowed)> \n'
+            + 'Registers/Updates the user to the bot. \n'
+            + '**!joinPlayer** <username (mention)> \n'
+            + 'Joins the user to the next tournament. \n'
+            + '**!leavePlayer** <username (mention)> \n'
+            + 'Removes the entry of the mentioned user for the next tournament. \n'
+            + '**!createtournament** (or !createteams) \n'
+            + 'This starts the process of creating the teams. \n'
+            + '**!showteams** *<live (optional)>* \n'
+            + 'This shows the teams created during !createtournament. \n'
+            + '*live:* When used with *live* parameter, this will show the teams in the Kayzr channel. \n'
+            + '**!endtournament** *<live (optional)> <showWinningTeam (optional)> <winningTeamNumber (required when using showWinningTeam)> <updateRoles (optional, but only with showWinningTeam)>* \n'
+            + 'This ends the current tournament, resets the "Joined Kayzr" roles and unjoins everyone from the playerlist. This also unsets Kayzr Alpha Winner roles. \n'
+            + '*live* Sends the message to the #kayzr channel. \n'
+            + '*showWinningTeam #* Shows the winning team. \n'
+            + '*updateRoles* Updates the Kayzr Alpha Winner role to the winning team.  \n'
+            + '**!showwinningteam** <teamnumber> *<live (optional)> <updateRoles (optional)>* \n'
+            + 'This shows the winning team, and updates the winners role accordingly if *updateroles* is given. \n'
         );
     }
     
-    message.author.send({embed});
-
-
     return;
 
 }
