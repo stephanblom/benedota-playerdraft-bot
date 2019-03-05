ccdfd56ab4553389bb6de6d0403cbf174b48b2b3
stephanblom <stephan_blom@live.nl>
2019-03-05 20:30:23 +0100

    Update help.js

--- bot/help.js
+++ bot/help.js
@@ -26,45 +26,46 @@ exports.sendHelp = function (message, args) {
     for (var key in helpCommands) {
         embed.addField(key, helpCommands[key]);
     }
-    message.channel.send({embed});
 
-    if ((message.member.roles.find("name", "Admin")
-        || message.author.id === '157938886784319489')
-        && args[0] == 'admin'
+    if (message.member.roles.find("name", "Admin")
+        || message.member.roles.find("name", "Staff")
     ) {
-        var embed = new Discord.RichEmbed()
-            .setColor('#a52b16')
-            .attachFile(`./images/dota2.png`)
-            .setThumbnail(`attachment://dota2.png`)
-            .setTimestamp()
-            .setTitle(`Extra commands for the Admins`)
-            .setDescription(`These are the currently available commands to use`)
-            .setFooter(`BeNeDota Kayzr Player Draft Help`);
-
         embed.addField(
             '!registerPlayer <username (mention)> <mmr> <position> <preferred captain>',
             'Registers/Updates the user to the bot'
         );
         embed.addField(
-            '!joinPlayer <username (mention)> <mmr> <position> <preferred captain>',
+            '!joinPlayer <username (mention)>',
             'Joins the user to the next tournament'
         );
         embed.addField(
-            '!leavePlayer <username (mention)> <mmr> <position> <preferred captain>',
+            '!leavePlayer <username (mention)>',
             'Removes the entry of the user for the next tournament'
         );
         embed.addField(
-            '!exportplayers (or: !exportjoinedplayers) <\'csv\' (optional)>',
-            `- This shows the joined players if started without 'csv' in csv format in chat. `
-             + `This starts the process of creating the teams if 'csv' parameter has been given. Caution with this!`
+            '!createtournament (or !createteams)',
+             `This starts the process of creating the teams.`
         );
         embed.addField(
             '!showteams <live (optional)>',
-            `This shows the teams created during \`!exportplayers csv\` in this chat. `
+            `This shows the teams created during !createtournament. `
              + `When used with 'live' parameter, this will show the teams in the Kayzr channel.`
-        )
-        message.channel.send({embed});
+        );
+        embed.addField(
+            '!endtournament <live (optional> <showWinningTeam (optional> <winningTeamNumber (required when using showWinningTeam)>',
+            `This ends the current tournament, resets the "Joined Kayzr" roles and unjoins everyone from the playerlist. `
+            + `This also unsets Kayzr Alpha Winner roles. `
+            + `**live** Sends the message to the #kayzr channel. `
+            + **showWinningTeam #** Gives the winners role to the winning team. `
+        );
+        embed.addField(
+            '!showwinningteam <teamnumber> <live (optional)> <updateRoles (optional)',
+            `This shows the winning team, and updates the winners role accordingly if *updateroles* is given. `
+        );
     }
+    
+    message.author.send({embed});
+
 
     return;
 
